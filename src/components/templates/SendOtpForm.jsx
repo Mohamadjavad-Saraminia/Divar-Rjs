6d1c08c7bed7da4c8010bb1e889eb4ccc10cbb88
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-05-19 17:07:27 +0330

    Tarahi Form For Get Mobile Number

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,7 +1,20 @@
 
-function SendOtpForm() {
+function SendOtpForm({ mobile, setMobile, setStep }) {
+  const submitHandeler = event => {
+    event.preventDefault();
+    console.log(event);
+  };
+
   return (
-    <div>SendOtpForm</div>
+    <form onSubmit={submitHandeler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید . کد تایید به این شماره پیامک خواهد شد .
+      </span>
+      <label htmlFor="input"> شماره موبایل خود را وارد بکنید</label>
+      <input type="text" id="input" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
+      <button type="submit">ارسال کد تایید</button>
+    </form>
   )
 }
 
